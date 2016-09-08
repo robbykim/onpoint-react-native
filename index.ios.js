1ca15c57498b6042f1190623a184c5612a840e66
Robby Kim <robert.st.kim@gmail.com>
2016-09-08 18:56:39 -0400

    commented index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -8,14 +8,25 @@ import {
   ActivityIndicatorIOS
 } from 'react-native';
 
+// module that allows swiping
 const Swiper = require('react-native-swiper');
+
+// Games component that creates each individual cell component
 const Games = require('./js/games');
+
+// Internal storage of all 32 NFL team IDs
 const teamStorage = require('./js/teamStorage');
+
+// API Client Token for Stattleship and CONSTANTS
 const CLIENT_KEY = '907bed7a31d8fa587d85ccea44e158c9';
 const FIRST_DAY = 1473220800000;
 const ONE_WEEK = 604800000;
 
+/** Primary Component for App */
 class OnPointFantasy extends Component {
+
+  // constructor function that holds the state for the games that were loaded
+  // and the whether or not the application is loading to display a loading Page
   constructor() {
     super();
     this.state = {
@@ -24,16 +35,28 @@ class OnPointFantasy extends Component {
     };
   }
 
+  // Fetch request happens in here where it will call the API and receive the
+  // current weeks schedule. it will then pass the list of games to this.getGames
   fetchSchedule() {
+    // Algorithm to get the current week
+    // starts with getting the current time in milliseconds
     const currentTime = new Date().getTime();
     let week = 1;
+
+    // subtracts the current time with the first day of the NFL season (Wed 9/7)
     let timeDiff = currentTime - FIRST_DAY;
+
+    // while the difference is greater than 1 week in ms, it will loop
     while (timeDiff > ONE_WEEK) {
+      // subtracts by a week every iteration and iterates what week it is
       timeDiff -= ONE_WEEK;
       week++;
     }
 
+    // API url to get the games of a given week. week (number) is calculated above
     const url = `https://api.stattleship.com/football/nfl/games?week=${week}`;
+    // passing specific headers that the api requires. API receives
+    // auth token only via a authorization header
     const init = {
       headers: {
         Accept: 'application/vnd.stattleship.com; version=1',
@@ -50,8 +73,12 @@ class OnPointFantasy extends Component {
       .catch(err => console.log(err));
   }
 
+  // getGames will take the games array from this.fetchSchedule and for each game,
+  // push an object into the games array and then set this.state.games to the new array
   getGames(games) {
     const gamesArray = [];
+
+    // grabbing the matching team names given a team id
     games.forEach(game => {
       const homeTeam = teamStorage.filter(team => {
         return team.id === game.home_team_id;
@@ -73,12 +100,14 @@ class OnPointFantasy extends Component {
       });
     });
 
+    // sets a new state and changes isLoading to false
     this.setState({
       games: gamesArray,
       isLoading: false,
     });
   }
 
+  // will render when isLoading is true
   renderLoadingMessage() {
     return (
       <View style={styles.loadingContainer}>
@@ -93,9 +122,14 @@ class OnPointFantasy extends Component {
     );
   }
 
+  // will render when isLoading is false
   renderResults() {
     const progressGames = [];
     const completedGames = [];
+
+    // for each game in the new state.games it will add to an array based off
+    // whether a game is completed or not. Games component creates each individual
+    // box score that will display on the screen
     this.state.games.forEach((game, index) => {
       if (game.status === 'ended') {
         completedGames.unshift(
@@ -136,10 +170,13 @@ class OnPointFantasy extends Component {
     );
   }
 
+  // when component first mounts, it will fetch the schedule
   componentDidMount() {
     this.fetchSchedule();
   }
 
+  // will determine if app is loading or not and run either
+  // this.renderLoadingMessage or this.renderResults
   render() {
     const { isLoading } = this.state;
     if (isLoading) {
@@ -150,6 +187,7 @@ class OnPointFantasy extends Component {
   }
 }
 
+// stylesheet for the Component
 const styles = StyleSheet.create({
   loadingContainer: {
     flex: 1,
